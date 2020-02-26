844b000e25b0d66b9a407a8e320a13d4119e01c3
Favour Chukwuedo <fchukwuedo@gmail.com>
2020-02-26 18:13:44 +0100

    First Commit

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'findit';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['pre'] = 'ad_';
+
+$config['version'] = '8.6';
+$config['installed'] = '1';
+?>
