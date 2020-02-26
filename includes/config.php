2792bb5ea37bb834105037621f090f93fa48e609
Favour Chukwuedo <fchukwuedo@gmail.com>
2020-02-26 18:22:23 +0100

    update1

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $config['db']['host'] = 'localhost';
-$config['db']['name'] = 'findit';
-$config['db']['user'] = 'root';
-$config['db']['pass'] = '';
+$config['db']['name'] = 'heroku_839c195b5522d8c';
+$config['db']['user'] = 'b077b9602abb25';
+$config['db']['pass'] = '4b9bfab2';
 $config['db']['pre'] = 'ad_';
 
 $config['version'] = '8.6';
